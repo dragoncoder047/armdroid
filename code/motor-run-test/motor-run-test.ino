580207878415471605d2dab0d9296e0dbd350b79
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-09-19 09:54:00 -0400

    revise code

--- code/motor-run-test/motor-run-test.ino
+++ code/motor-run-test/motor-run-test.ino
@@ -21,15 +21,32 @@ const unt8_t steps[8] = {
 
 // State
 uint8_t index = 0;
+uint64_t lastStep = 0;
 
 void setup() {
     pinMode(PA, OUTPUT); pinMode(PB, OUTPUT); pinMode(PC, OUTPUT); pinMode(PD, OUTPUT);
     pinMode(CTL, INPUT);
 }
+
 void loop() {
+    uint32_t val = analogRead(CTL);
+    // delay the required amount
+    // Math here: 1000000 us   / 400 half-steps   X turns \    X us
+    //            ---------- / | -------------- x ------- | == ----
+    //               sec       \      turn          sec   /    step
+    int64_t speed = (1000000L / 400) / (int32_t)MAPF((float)val, 0, 1023.0, -10.0, 10.0);
+    uint64_t delay_amount = abs(speed); // abs is a macro; let's save some calculations
+    uint64_t mi = micros();
+    if (lastStep + delay_amount >= mi) {
+        lastStep = mi;
+        step(speed > 0);
+    }
+}
+
+void step(bool forward) {
     // update index
-    if (analogRead(CTL) < (1023 / 2)) index = (index + 1) & 7; // &7 same as %8 but faster
-    else                                    index = (index + 7) & 7; // +7 is equivalent to -1 in mod-8
+    if (forward) index = (index + 1) & 7; // &7 same as %8 but faster
+    else         index = (index + 7) & 7; // +7 is equivalent to -1 in mod-8
 
     // update pins
     uint8_t s = steps[index];
@@ -37,11 +54,4 @@ void loop() {
     digitalWrite(PB, s & 0b0010);
     digitalWrite(PC, s & 0b0100);
     digitalWrite(PD, s & 0b1000);
-
-    // delay the required amount
-    // Math here: 1000000 us   / 400 half-steps   X turns \    X us
-    //            ---------- / | -------------- x ------- | == ----
-    //               sec       \      turn          sec   /    step
-    int32_t delay_amount = (1000000L / 400) / (int32_t)MAPF((float)analogRead(CTL), 0, 1023.0, -10.0, 10.0);
-    delayMicroseconds(abs(delay_amount));
 }
--- code/motor-run-test/readme.md
+++ code/motor-run-test/readme.md
@@ -1,7 +1,7 @@
 This code was used with a ULN2003A on a breadboard to test the motors.
 
-Connect pins 3, 4, 5, and 6 of the Arduino (or whatever you choose, edit the `#define`s at the top) to four input channels of the ULN2003A, and the output channels to the four leftmost pins on the motor's plug.
+Connect pins 3, 4, 5, and 6 of the Arduino (or whatever you choose, edit the `#define`s at the top) to four input channels of the ULN2003A, and the output channels to the four leftmost pins on the motor's plug. Alternatively you can just use four transistors if you don't have a ULN2003A -- I like the 2N7000.
 
 Connect the remaining (rightmost) pin of the motor plug, pin 9 of the ULN2003A ("flybacks common cathode"), and the Arduino's VIN pin to +12V, and the Arduino ground to pin 8 of the ULN2003A and power supply ground.
 
-Connect the ends of a potentiometer to +5V and GND coming off the Arduino, and the wiper to pin A0. This is the speed control.
+Pin A0 is the control voltage which affects the motor speed and direction. Connecting it to GND will try to run the motor at 600 RPM backwards, while +5V will try to run it at 600 RPM forwards. You can use a potetntionmeter to vary the speed in-between.
