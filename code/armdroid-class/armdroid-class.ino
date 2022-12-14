ba39e4b3174d65a6cb8c7dec1d307f428192a350
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-12-14 12:08:17 -0500

    use joysticks driver

--- code/armdroid-class/armdroid-class.ino
+++ code/armdroid-class/armdroid-class.ino
@@ -4,59 +4,31 @@
 #define PB FILL_ME_IN
 #define PC FILL_ME_IN
 #define PD FILL_ME_IN
+#define P0 FILL_ME_IN
 #define P1 FILL_ME_IN
 #define P2 FILL_ME_IN
-#define P3 FILL_ME_IN
 #define PK FILL_ME_IN
 
-Armdroid arm(PA, PB, PC, PD, P1, P2, P3, PK);
-
-uint8_t watching = 0;
-uint8_t i;
-const char s[8] = 'BSELRGAJ';
+Armdroid arm(PA, PB, PC, PD, P0, P1, P2, PK);
 
 void setup() {
-    Serial.begin(9600);
+    // nothing
+    pinMode(A0, INPUT);
+    pinMode(A1, INPUT);
+    pinMode(A2, INPUT);
+    pinMode(A3, INPUT);
+    pinMode(A4, INPUT);
+    pinMode(A5, INPUT);
 }
+#define mmm(N) r = analogRead(A##N); if (abs(r - 512) > 50) arm.motorMoveby(N, analogRead(A##N) > 512 ? 2 : -2);
 void loop() {
     arm.tick();
     delay(10); // don't move the motors too fast
-    long pos = 0;
-    uint8_t motor = 255;
-    if (Serial.available()) {
-        char c = Serial.peek();
-        while ('0' <= c && c <= 9) {
-            pos *= 10;
-            pos += c - '0';
-            c = Serial.peek();
-        }
-        for (i = 0; i < 8; i++) {
-            if (s[i] == (c | 0x20)) { // allow uppercase or lowercase
-                motor = i;
-                break;
-            }
-        }
-        if (motor == 255) {
-            Serial.println('Invalid Motor');
-            Serial.flush();
-            return;
-        }
-        watching |= 1 << motor;
-        Serial.print("Moving Motor ");
-        Serial.write(s[i]);
-        Serial.print(" from position ");
-        Serial.print(arm.getPos(i));
-        Serial.print(" to position ");
-        Serial.println(pos);
-        arm.motorGoto(motor, pos);
-    }
-    for (i = 0; i < 8; i++) {
-        if (watching & (1 << i) && arm.isStopped(i)) {
-            watching &= ~(1 << i);
-            Serial.print("Motor ");
-            Serial.write(s[i]);
-            Serial.print(" is now at position ");
-            Serial.println(arm.getPos(i));
-        }
-    }
+    int r;
+    mmm(0);
+    mmm(1);
+    mmm(2);
+    mmm(3);
+    mmm(4);
+    mmm(5);
 }
