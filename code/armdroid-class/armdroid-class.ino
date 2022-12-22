e3a06f126303348ef7417b2f02142f38c2dbaaa3
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-12-22 10:04:34 -0500

    Update armdroid-class.ino

--- code/armdroid-class/armdroid-class.ino
+++ code/armdroid-class/armdroid-class.ino
@@ -1,18 +1,19 @@
 #include "armdroid.cpp"
 
-#define PA FILL_ME_IN
-#define PB FILL_ME_IN
-#define PC FILL_ME_IN
-#define PD FILL_ME_IN
-#define P0 FILL_ME_IN
-#define P1 FILL_ME_IN
-#define P2 FILL_ME_IN
-#define PK FILL_ME_IN
+#define PA 8
+#define PB 9
+#define PC 6
+#define PD 7
+#define P0 2
+#define P1 5
+#define P2 4
+#define PK 3
 
 Armdroid arm(PA, PB, PC, PD, P0, P1, P2, PK);
 
 void setup() {
     // nothing
+    Serial.begin(9600);
     pinMode(A0, INPUT);
     pinMode(A1, INPUT);
     pinMode(A2, INPUT);
@@ -20,16 +21,18 @@ void setup() {
     pinMode(A4, INPUT);
     pinMode(A5, INPUT);
 }
-#define mmm(N) r = analogRead(A##N); if (abs(r - 512) > 50) arm.motorMoveby(N, r > 512 ? 2 : -2);
+#define mmm(N) r = analogRead(A##N); if (abs(r - 512) > 50) arm.motorMoveby(N, r > 512 ? 2 : -2); Serial.print(r); Serial.write('\t'); 
 void loop() {
     arm.tick();
-    arm.tick(); // tick twice to do full stepping
-    delay(10); // don't move the motors too fast
+    arm.tick();
+    delay(2); // don't move the motors too fast
     int r;
+    Serial.println("m0\tm1\tm2\tm3\tm4\tm5");
     mmm(0);
     mmm(1);
     mmm(2);
     mmm(3);
     mmm(4);
     mmm(5);
+    Serial.write('\n');
 }
